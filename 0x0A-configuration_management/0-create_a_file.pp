af19acf99e16748a75c3ece03497eb76c67e99ab
theurikenneth <theurikenneth@gmail.com>
2021-08-15 14:26:13 +0000

    Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 file { '/tmp/holberton':
   ensure  => file,
   path    => '/tmp/holberton',
-  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www.data',
   mode    => '0744',
+  content => 'I love Puppet',
 }
