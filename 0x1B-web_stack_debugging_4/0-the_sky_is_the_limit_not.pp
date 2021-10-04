30a9ebd3de40776b3db1e35a0b55deaf0d283119
theurikenneth <theurikenneth@gmail.com>
2021-10-04 09:41:50 +0000

    Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# set open file limit higher
+exec { 'set limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/15/2000/' /etc/default/nginx"
+}
+exec { 'reboot nginx':
+  command => '/usr/sbin/service nginx restart'
+}
